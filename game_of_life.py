2dc0c856301e9512790a6410a97372f51ecf38c7
Liam Cafearo <lcafearo@gmail.com>
2018-03-22 06:53:05 +0000

    amended function that populates cells so that no errors occur. This meant passing density rather than population to the function

--- game_of_life.py
+++ game_of_life.py
@@ -1,10 +1,9 @@
+import random
 import sys
 
 import pygame
 
-import random
-
-from colours import dark_blue
+from colours import dark_blue, green, black
 
 
 def draw_grid():
@@ -14,27 +13,30 @@ def draw_grid():
         pygame.draw.line(screen, dark_blue, (0, y), (width, y))
 
 
-def get_cells(population=100):
-    cells = {(c, r): False for c in range(columns) for r in range(rows)}
-    for i in range(population):
-        col = random.randint(0, columns)
-        row = random.randint(0, rows)
-        cells[col, row] = True
-    return cells
+def get_cells(density=0.2):
+    return {(c, r): random.random() < density for c in range(columns) for r in range(rows)}
+
+
+def draw_cells():
+    for (x, y) in cells:
+        colour = green if cells[x, y] else black
+        rectangle = (x * cell_size, y * cell_size, cell_size, cell_size)
+        pygame.draw.rect(screen, colour, rectangle)
 
 
 pygame.init()
-
 columns, rows = 100, 50
 cell_size = 10
 size = width, height = columns * cell_size, rows * cell_size
 screen = pygame.display.set_mode(size)
+cells = get_cells()
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
 
+    draw_cells()
     draw_grid()
 
     pygame.display.update()
